743c607b612d2cf8bdc6d6ef08b085006bf4ed26
cedricvasset <vasset.cedric@orange.fr>
2018-12-14 16:04:41 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+<head>
+  <meta charset="utf-8" />
+  <title>php part1 ex4</title>
+  <link rel="stylesheet" href="style.css" />
+</head>
+<body>
+  <p>
+    <?php
+    $firstname='Cédric';
+    $int= 44;
+    $float= 75.8;
+    $bool = true;
+    echo 'je m\'appelle ' . $firstname . ' j\'ai ' . $int . ' ans et dans mes rèves je pèse ' . $float . ' tout cela est True ;) ' . $bool;
+    ?>
+  </p>
+</body>
+</html>
